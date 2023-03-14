dbe6834e5512af642b106aa5ff781cb9f5b032e6
Cameron Courtney <cameroncourtney@utexas.edu>
2023-03-14 02:05:35 -0500

    Fixed Naming of data

--- Data/Past/Combine/merge.py
+++ Data/Past/Combine/merge.py
@@ -7,5 +7,10 @@ year_p_df = pd.read_csv('p-data.csv', dtype={'Year': int}, thousands=',')
 # Merge the dataframes on the 'Team' column
 merged_df = pd.merge(year_df, year_p_df[['Team', 'Year', 'Rk']], on=['Team', 'Year'], how='left')
 
+# Rename columns after merge.
+# Rk is the T-Rank of a team considering the entire season and confernce tournaments.
+# P-Rk is the T-Rank of a team solely considering their conference tournament performance.
+merged_df = merged_df.rename({'Rk_x': 'Rk', 'Rk_y': 'P-Rk'}, axis=1) 
+
 # Export the final dataframe to a CSV file
 merged_df.to_csv('final-data.csv', index=False)
--- Data/Past/Combine/names.py
+++ Data/Past/Combine/names.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+# Figuring out which names don't match in the two files.
+# After this, went through and manually altered names.
+
 # read the two csv files
 data = pd.read_csv('data.csv')['Team'].unique()
 names = pd.read_csv('MTeams.csv')['TeamName'].unique()
@@ -9,3 +12,17 @@ not_in_names = set(data) - set(names)
 
 # print the names that are not in names.csv
 print(list(not_in_names))
+
+# Result
+# ['Stephen F. Austin', 'Arizona St.', "Saint Mary's", 'Norfolk St.', 'College of Charleston', 'Georgia St.', 
+# 'Green Bay', 'Loyola Chicago', 'Kansas St.', 'Little Rock', 'Appalachian St.', 'Boise St.', 'Wichita St.', 
+# 'Colorado St.', 'New Mexico St.', 'St. Bonaventure', 'Eastern Washington', 'Montana St.', 'Fresno St.', 
+# 'Jacksonville St.', 'Iowa St.', 'Coastal Carolina', 'Texas Southern', 'North Carolina Central', 
+# 'Oklahoma St.', "Saint Joseph's", 'Southern', 'Weber St.', 'Texas A&M Corpus Chris', 'San Diego St.', 
+# 'Prairie View A&M', 'Murray St.', 'Florida St.', 'Cal St. Bakersfield', 'Kent St.', 'Saint Louis',
+#  'Fairleigh Dickinson', 'Morehead St.', 'Albany', "Saint Peter's", 'Oregon St.', 'Cleveland St.',
+#  'North Carolina St.', 'Utah St.', 'North Dakota St.', "Mount St. Mary's", 'Northern Kentucky', 
+# 'Michigan St.', 'South Dakota St.', 'Middle Tennessee', 'East Tennessee St.', 'Cal St. Fullerton', 'Wright St.', 'Mississippi St.', 'Ohio St.', "St. John's"]
+
+# Second iteration
+# ['Middle Tennessee', 'Southern', 'East Tennessee St.']
